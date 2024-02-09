da6fe880dd38d1d693bbc7f77096b8f323b6583d
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-02-10 01:06:08 +0530

    Second Commit

--- 1000 Level/theature_square.cpp
+++ 1000 Level/theature_square.cpp
@@ -1,11 +1,25 @@
+/*
+Not fully solved
+
+
+*/
+
+
+
 # include <iostream>
 using namespace std;
 int main()
 {
     int a,b,c,d;
     cin>>a>>b>>c;
-    a = a/c;
-    b = b/c;
-    cout<<(a+b)*2;
+    a = a+(a%c);
+    b = b+(b%c);
+    if((a*b)>=(c*c))
+    {
+    cout<<(a*b)/(c*c);
+    }
+    else{
+        cout<<1;
+    }
     
 }
