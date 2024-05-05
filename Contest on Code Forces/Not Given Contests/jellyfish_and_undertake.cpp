e6c3909e940c95f684921265c2f23258c12ca697
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-05-05 15:43:09 +0530

    Day 12 of solving 10 Problems a day

---
+++ Contest on Code Forces/Not Given Contests/jellyfish_and_undertake.cpp
@@ -0,0 +1,23 @@
+# include <bits/stdc++.h>
+// solved
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        vector <int> arr(c);
+        long long int answer = b;
+        for (int j =0; j<c; j++)
+        {
+            cin>>arr[j];
+            answer+=min(a-1,arr[j]);
+        }
+        
+        
+        cout<<answer<<endl;
+    }
+}
